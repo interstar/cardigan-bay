d7f77353490825879debc1c035c40dd678f72dc4
interstar <interstar@gmail.com>
2020-04-03 12:59:00 -0300

    adding unit testing

---
+++ test/clj_ts/core_test.clj
@@ -0,0 +1,34 @@
+(ns clj-ts.core-test
+  (:require [clojure.test :refer :all]
+            [clj-ts.common :as common]
+            ))
+
+
+(deftest basic-cards
+  (testing "Making and analysing cards"
+    (let [c (common/package-card 1 :markdown :another "hello teenage america")]
+      (is (= (:type c) :markdown))
+      (is (= (:delivered_type c) :another))
+      (is (= (:id c) 1))
+      (is (= (:data c) "hello teenage america")))))
+
+
+(deftest basic-search-and-replace
+  (testing "Searching cards"
+    (let [cards [(common/package-card 1 :a :b "hello")
+                 (common/package-card 2 :a :b "teenage")
+                 (common/package-card 3 :a :b "america")]
+          h (-> cards second :hash)
+          new-card (common/package-card 22 :b :a "boomer")
+          h2 (:hash new-card)
+          c2 (common/sub-card cards #(= 2 (:id %)) new-card )
+          nc2 (common/package-card 33 :c :d "brazil")
+          c3 (common/sub-card cards #(= 4 (:id %)) nc2)]
+      (is (= 2 (->
+                (common/find-card-by-hash cards h)
+                :id)))
+      (is (nil? (common/find-card-by-hash cards "777")))
+      (is (= 3 (count c2)))
+      (is (= 22 (-> (common/find-card-by-hash c2 h2) :id)))
+      (is (= c3 cards))
+)))
