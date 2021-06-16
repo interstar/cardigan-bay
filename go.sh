27dd741775d6758c3708d45d4664c71abc4f6ad1
phil jones <interstar@gmail.com>
2021-06-16 16:31:56 -0300

    fixed double argument

--- go.sh
+++ go.sh
@@ -1,1 +1,1 @@
-clj -A:dev:app -n "Bedrock!" -s "https://my-cardiganbay.com/" -p 4545 -i "AboutThisWiki" -l "./" -x "" -l "https://my-cardiganbay.com/view/"
+clj -A:dev:app -n "Bedrock!" -s "https://example.com/" -p 4545 -i "AboutThisWiki" -l "./" -x "" 
